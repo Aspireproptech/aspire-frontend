b313913b489401ccce22e218c0aa51d40062c195
mdmahfujarrahman <ahmedmahfuj28@gmail.com>
2023-04-14 16:42:06 +0600

    call and whats app added

--- src/Components/Common/WhatsApps.jsx
+++ src/Components/Common/WhatsApps.jsx
@@ -1,13 +1,22 @@
 const WhatsApps = () => {
     return (
-        <a
-            href="https://wa.me/919886660229?text=Hello!%20Thank%20you%20for%20being%20so%20interested%20in%20our%20property.%20Please%20let%20us%20know%20what%20information%20you%20would%20like%20to%20learn%20more%20about.%20We're%20happy%20to%20help!"
-            class="whatsapp_float d-md-block d-lg-none"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            <i class="fa fa-whatsapp whatsapp-icon"></i>
-        </a>
+        <div class="whatsapp_float d-md-block d-lg-none">
+            <a href="tel:+919886660229" className="callNow">
+                <i class="fa-solid fa-phone"></i>
+                <span>Call Now</span>
+            </a>
+            <div
+                onClick={() =>
+                    window.open(
+                        "https://wa.me/919886660229?text=Hi%20there.%20I%20am%20on%20the%20lookout%20for%20my%20dream%20home%2C%20and%20I%20came%20across%20Aspire%20Proptech%20website.%20Can%20you%20please%20assist%20me%20in%20finding%20the%20perfect%20home%3F"
+                    )
+                }
+                className="whatsApp"
+            >
+                <i class="fa fa-whatsapp"></i>
+                <span>Whatsapp</span>
+            </div>
+        </div>
     );
 };
 
